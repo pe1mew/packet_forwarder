5ff172a403d7cc419a9e34d4a11c642a50b8be64
Remko <pe1mew@gmail.com>
2020-04-04 16:06:22 +0200

    Added files for conversion functions

---
+++ mp_pkt_fwd/inc/heliumeuus.h
@@ -0,0 +1,32 @@
+/*--------------------------------------------------------------------
+  This file is part of the mp_pktfwd cross conversion function library.
+
+  This code is free software:
+  you can redistribute it and/or modify it under the terms of a Creative
+  Commons Attribution-NonCommercial 4.0 International License
+  (http://creativecommons.org/licenses/by-nc/4.0/) by
+  Remko Welling (http://rfsee.nl) E-mail: remko@rfsee.nl
+
+  The program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  --------------------------------------------------------------------*/
+
+/*!
+ * \file heliumeuus.h
+ * \brief cross conversion functions EU868 US915 freuencies for the Helium network.
+ * \author R. Welling (remko@rfsee.nl)
+ * \date See version history
+ * \version See version history
+ *
+ * Version|Date        |Note
+ * -------|------------|----
+ * 0      | 4-4-2020   | Initial version 
+ * 
+ */
+
+#ifndef _HELIUM_EU_US_H_
+#define _HELIUM_EU_US_H_
+
+
+#endif /* _HELIUM_EU_US_H_ */
---
+++ mp_pkt_fwd/src/heliumeuus.c
@@ -0,0 +1,19 @@
+/*--------------------------------------------------------------------
+  This file is part of the mp_pktfwd cross conversion function library.
+
+  This code is free software:
+  you can redistribute it and/or modify it under the terms of a Creative
+  Commons Attribution-NonCommercial 4.0 International License
+  (http://creativecommons.org/licenses/by-nc/4.0/) by
+  Remko Welling (http://rfsee.nl) E-mail: remko@rfsee.nl
+
+  The program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  --------------------------------------------------------------------*/
+
+/// \file heliumeuus.c
+/// \author R. Welling (remko@rfsee.nl)
+
+#include heliumeuus.h
+
